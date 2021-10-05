ce8658d51768822b1e9bf0ff13a09918ac2a491a
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-04 18:52:15 -0700

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace cd_c_timeDisplay
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ADD THIS LINE -->
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // ADD THIS LINE, REPLACING USEROUTING & USEENDPOINTS -->
+            app.UseMvc();
+            
+            // app.UseRouting();
+
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapGet("/", async context =>
+            //     {
+            //         await context.Response.WriteAsync("Hello World!");
+            //     });
+            // });
+        }
+
+        // ADD THESE LINES -->
+        public Startup(IWebHostEnvironment env)
+        {
+            System.Console.WriteLine(env.ContentRootPath);
+            System.Console.WriteLine(env.IsDevelopment());
+        }
+    }
+}
